ed684c91234b781b5d966746f94905319faae168
brucethe1 <159022563+brucethe1@users.noreply.github.com>
2025-03-29 23:08:48 +0200

    Create fetchPosts.js

---
+++ react-query-demo/src/components/fetchPosts.js
@@ -0,0 +1,8 @@
+// src/utils/fetchPosts.js
+export const fetchPosts = async () => {
+  const response = await fetch('https://jsonplaceholder.typicode.com/posts');
+  if (!response.ok) {
+    throw new Error('Network response was not ok');
+  }
+  return response.json();
+};
