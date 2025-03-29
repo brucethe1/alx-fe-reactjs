86312e1e1d4b27502e28b3e709c1f25605c9f1b7
brucethe1 <159022563+brucethe1@users.noreply.github.com>
2025-03-30 01:15:00 +0200

    Create Profiles.jsx

---
+++ react-router-advanced/src/components/Profiles.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import { Routes, Route, Link } from 'react-router-dom';
+import "./ ProfileSettings";
+import "./ProfileDetails"
+import "./  Home"
+
+function Profile() {
+  return (
+    <div>
+      <h2>Profile Page</h2>
+      <nav>
+        <ul>
+          <li>
+            <Link to="details">Profile Details</Link>
+          </li>
+          <li>
+            <Link to="settings">Profile Settings</Link>
+          </li>
+        </ul>
+      </nav>
+      <Routes>
+        <Route path="details" element={<ProfileDetails />} />
+        <Route path="settings" element={<ProfileSettings />} />
+      </Routes>
+    </div>
+  );
+}
+
+export default Profile;
