32fd5dadb52e961d45a365c11e3854286d88a037
brucethe1 <159022563+brucethe1@users.noreply.github.com>
2025-04-10 09:42:32 +0200

    Update App.jsx

--- recipe-sharing-app/src/App.jsx
+++ recipe-sharing-app/src/App.jsx
@@ -1,25 +1,24 @@
 // src/App.jsx
-import { Routes, Route ,Router} from 'react-router-dom';
-import RecipeList from './components/RecipeList';
 import AddRecipeForm from './components/AddRecipeForm';
+import RecipeList from './components/RecipeList';
 import RecipeDetails from './components/RecipeDetails';
-
+import SearchBar from './components/SearchBar';
+import { useRecipeStore } from './store/recipeStore';
 
 const App = () => {
+  const selectedRecipe = useRecipeStore((state) => state.selectedRecipe);
+
   return (
-    <div>
+    <div className="app-container" style={{ padding: '1rem', maxWidth: '800px', margin: 'auto' }}>
       <h1>Recipe Sharing App</h1>
-      <Routes>
-        <Router>
-        <Route path="/" element={
-          <>
-            <AddRecipeForm />
-            <RecipeList />
-          </>
-        } />
-        <Route path="/recipes/:id" element={<RecipeDetails />} />
-        </Router>
-      </Routes>
+      {!selectedRecipe && (
+        <>
+          <SearchBar />
+          <AddRecipeForm />
+          <RecipeList />
+        </>
+      )}
+      {selectedRecipe && <RecipeDetails />}
     </div>
   );
 };
