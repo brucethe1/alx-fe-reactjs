d2be713a8b9469dcd7d56afde994a573ca277f8d
brucethe1 <159022563+brucethe1@users.noreply.github.com>
2025-04-11 19:30:23 +0200

    Update tailwind.config.js

--- github-user-search/tailwind.config.js
+++ github-user-search/tailwind.config.js
@@ -1,10 +1,10 @@
 module.exports = {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  // ... other config
   theme: {
-    extend: {},
-  },
-  plugins: [],
+    extend: {
+      animation: {
+        spin: 'spin 1s linear infinite',
+      }
+    }
+  }
 }
