b3279c7fe0bf5b77aba36a8d7824e46b38dcabf0
brucethe1 <159022563+brucethe1@users.noreply.github.com>
2025-04-11 22:29:59 +0200

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,12 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
-import './index.css'; // <- THIS LINE IS REQUIRED!
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App'
+import './index.css'  // 👈 Import here (right under App import)
+import reportWebVitals from './reportWebVitals' // (if exists)
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<App />);
+const root = ReactDOM.createRoot(document.getElementById('root'))
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+)
