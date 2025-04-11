6c7c54a9b4b900ba73172eaa9396f3a5c841ef52
brucethe1 <159022563+brucethe1@users.noreply.github.com>
2025-04-11 09:57:52 +0200

    Update recipeStore.js

--- recipe-sharing-app/src/store/recipeStore.js
+++ recipe-sharing-app/src/store/recipeStore.js
@@ -1,8 +1,13 @@
 import create from 'zustand';
 
+// Zustand store to manage recipes
 const useRecipeStore = create((set) => ({
   recipes: [],
+  
+  // Function to add a new recipe
   addRecipe: (newRecipe) => set((state) => ({ recipes: [...state.recipes, newRecipe] })),
+  
+  // Function to set the list of recipes directly
   setRecipes: (recipes) => set({ recipes }),
 }));
 
