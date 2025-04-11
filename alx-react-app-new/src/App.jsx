72fc2d10166a9a92aa38fbb10b2fdd92cbf145d3
brucethe1 <159022563+brucethe1@users.noreply.github.com>
2025-04-11 10:29:58 +0200

    Update App.jsx

--- alx-react-app-new/src/App.jsx
+++ alx-react-app-new/src/App.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
-import Header from './Header';
-import UserProfile from './UserProfile';
-import MainContent from './MainContent';
-import Footer from './Footer';
+import Header from './components/Header';
+import UserProfile from './components/UserProfile';
+import MainContent from './components/MainContent';  // Import MainContent here
+import Footer from './components/Footer';
 
 const App = () => {
   return (
