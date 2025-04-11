edfcad4294d84fc5c5581ac3f7f8d658d96df6e4
brucethe1 <159022563+brucethe1@users.noreply.github.com>
2025-04-11 23:01:01 +0200

    Update  UserContext.js

--- alx-react-app-props/src/components/ UserContext.js
+++ alx-react-app-props/src/components/ UserContext.js
@@ -1,13 +1,5 @@
-// UserProfile.jsx
-function UserProfile(props) {
-    return (
-      <div style={{ border: '1px solid gray', padding: '10px', margin: '10px', maxWidth: '300px' }}>
-        <h2 style={{ color: 'blue', fontSize: '24px' }}>{props.name}</h2>
-        <p>Age: <span style={{ fontWeight: 'bold' }}>{props.age}</span></p>
-        <p>Bio: {props.bio}</p>
-      </div>
-    );
-  }
-  
-  export default UserProfile;
-  
+import { createContext } from 'react';
+
+const UserContext = createContext(null);
+
+export default UserContext;
