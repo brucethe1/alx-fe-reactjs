ab3a19698938d97a85b71aa36ba95811f5e08de2
brucethe1 <159022563+brucethe1@users.noreply.github.com>
2025-04-11 19:13:35 +0200

    Update App.jsx

--- github-user-search/src/App.jsx
+++ github-user-search/src/App.jsx
@@ -1,19 +1,10 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Home from './ pages/ Home';
-import UserDetail from './ pages/UserDetail';
+import Search from './components/Search';
 
 function App() {
   return (
-    <Router>
-      <div className="min-h-screen bg-gray-50 py-8 px-4">
-        <div className="max-w-4xl mx-auto">
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/user/:username" element={<UserDetail />} />
-          </Routes>
-        </div>
-      </div>
-    </Router>
+    <div className="min-h-screen bg-gray-100 py-8 px-4">
+      <Search />
+    </div>
   );
 }
 
