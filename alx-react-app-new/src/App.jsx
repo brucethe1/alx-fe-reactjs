674335bcef42aa88b9911f9782821bcf3890ed12
brucethe1 <159022563+brucethe1@users.noreply.github.com>
2025-04-11 10:32:36 +0200

    Update App.jsx

--- alx-react-app-new/src/App.jsx
+++ alx-react-app-new/src/App.jsx
@@ -3,10 +3,15 @@ import Header from './components/Header';
 import UserProfile from './components/UserProfile';
 import MainContent from './components/MainContent';  // Import MainContent here
 import Footer from './components/Footer';
+import Counter from './components/Counter'; 
 
 const App = () => {
   return (
     <div>
+      <div>
+      <h1>React Counter Application</h1>
+      <Counter /> {/* Include Counter component */}
+    </div>
       <Header />
       <UserProfile name="John Doe" age={28} bio="A software developer who loves traveling and exploring new cities." />
       <MainContent />
