f99853ae7f0861fae9089b4a5d4ea7dc9e4ca87d
brucethe1 <159022563+brucethe1@users.noreply.github.com>
2025-04-11 15:54:33 +0200

    Update App.jsx

--- alx-react-app-props/src/App.jsx
+++ alx-react-app-props/src/App.jsx
@@ -1,13 +1,13 @@
-import React from 'react';
-import { useUserContext } from './UserContext';
-
 import ProfilePage from './ProfilePage';
+import UserContext from './UserContext';
 
 function App() {
+  const userData = { name: "Jane Doe", email: "jane.doe@example.com" };
+
   return (
-    <UserProvider>
+    <UserContext.Provider value={userData}>
       <ProfilePage />
-    </UserProvider>
+    </UserContext.Provider>
   );
 }
 
