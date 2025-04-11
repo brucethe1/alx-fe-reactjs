8bdbcad4c26a17abe8288aed6a1372af88ed5572
brucethe1 <159022563+brucethe1@users.noreply.github.com>
2025-04-11 11:05:56 +0200

    Update App.jsx

--- alx-react-app-props/src/App.jsx
+++ alx-react-app-props/src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { UserProvider } from './UserContext'; // Import the UserProvider
+import  UserContext  from './UserContext'; // Import the UserProvider
 import ProfilePage from './ProfilePage';
 
 function App() {
