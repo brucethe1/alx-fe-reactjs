6358090710bb9de4569c2168b9cf1e56aed26c3b
brucethe1 <159022563+brucethe1@users.noreply.github.com>
2025-04-11 10:08:41 +0200

    Update App.jsx

--- recipe-sharing-app/src/App.jsx
+++ recipe-sharing-app/src/App.jsx
@@ -1,26 +1,27 @@
-import { useEffect } from 'react';
-import { useRecipeStore } from './store/recipeStore'; // Assuming store is in 'store/recipeStore.js'
-import RecipeList from './components/RecipeList';
-import AddRecipeForm from './components/AddRecipeForm';
+import React, { useEffect } from 'react';
+import { useRecipeStore } from './recipeStore';
+import { RecipeList } from './RecipeList';
+import { AddRecipeForm } from './AddRecipeForm';
+import { FavoritesList } from './FavoritesList';
+import { RecommendationsList } from './RecommendationsList';
 
-const RecipeApp = () => {
-  const setRecipes = useRecipeStore((state) => state.setRecipes);
-  
+const App = () => {
+  const recipes = useRecipeStore(state => state.recipes);
+  const generateRecommendations = useRecipeStore(state => state.generateRecommendations);
+
   useEffect(() => {
-    // Fetch recipes from an API and set them using setRecipes
-    fetch('https://someapi.com/recipes')
-      .then((response) => response.json())
-      .then((data) => {
-        setRecipes(data); // Set the recipes list
-      });
-  }, [setRecipes]); // Runs once when the component mounts
+    generateRecommendations();
+  }, [recipes]);
 
   return (
     <div>
-      <h1>Recipe Sharing App</h1>
-      {/* Add your Recipe List component here */}
+      <h1>Recipe Sharing Application</h1>
+      <AddRecipeForm />
+      <RecipeList />
+      <FavoritesList />
+      <RecommendationsList />
     </div>
   );
 };
 
-export default RecipeApp;
+export default App;
