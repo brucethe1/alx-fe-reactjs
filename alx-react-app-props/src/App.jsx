ef6586831b9024ed03e7ecd2b699c473f40e6db5
brucethe1 <159022563+brucethe1@users.noreply.github.com>
2025-04-13 11:14:38 +0200

    Update App.jsx

--- alx-react-app-props/src/App.jsx
+++ alx-react-app-props/src/App.jsx
@@ -1,7 +1,7 @@
 // alx-react-app-props/App.jsx
 import ProfilePage from './ProfilePage';
 import UserContext from './UserContext';
-
+import userinf from './userinfo'
 function App() {
   const userData = { name: "Jane Doe", email: "jane.doe@example.com" };
 
