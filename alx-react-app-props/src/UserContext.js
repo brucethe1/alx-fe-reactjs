282a5efaf24293fcdad3cff2fc687e008b51b4e0
brucethe1 <159022563+brucethe1@users.noreply.github.com>
2025-04-13 11:42:17 +0200

    Update UserContext.js

--- alx-react-app-props/src/UserContext.js
+++ alx-react-app-props/src/UserContext.js
@@ -1,5 +1,5 @@
-// alx-react-app-props/UserContext.js
-import { createContext } from "react";
+// src/UserContext.js
+import { createContext } from 'react';
 
 const UserContext = createContext(null);
 
