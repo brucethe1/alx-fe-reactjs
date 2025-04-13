2a58d06dc5a23cd447b157a0bd3783e2bee98619
brucethe1 <159022563+brucethe1@users.noreply.github.com>
2025-04-13 11:17:29 +0200

    Update UserContext.js

--- alx-react-app-props/src/UserContext.js
+++ alx-react-app-props/src/UserContext.js
@@ -1,1 +1,8 @@
 
+// alx-react-app-props/UserContext.js
+import { createContext } from 'react';
+
+// Create and export the context
+const UserContext = createContext(null);
+
+export default UserContext;
